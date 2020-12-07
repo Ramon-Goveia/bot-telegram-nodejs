30b81406085a9f5191572b757e1b47a96085c26b
Ramon-Goveia <ramonalangoveia@hotmail.com>
2020-12-07 09:59:03 -0300

    Configuração projeto base

--- dialogflow.js
+++ dialogflow.js
@@ -1,40 +1,45 @@
+// importando a biblioteca do dialogflow
 const dialogflow = require('dialogflow');
-const uuid = require('uuid');
- 
-/**
- * Send a query to the dialogflow agent, and return the query result.
- * @param {string} projectId The project to be used
- */
-async function runSample(projectId = 'your-project-id') {
-  // A unique identifier for the given session
-  const sessionId = uuid.v4();
- 
-  // Create a new session
-  const sessionClient = new dialogflow.SessionsClient();
-  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
- 
-  // The text query request.
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        // The query to send to the dialogflow agent
-        text: 'hello',
-        // The language used by the client (en-US)
-        languageCode: 'en-US',
-      },
-    },
-  };
- 
-  // Send request and log result
-  const responses = await sessionClient.detectIntent(request);
-  console.log('Detected intent');
-  const result = responses[0].queryResult;
-  console.log(`  Query: ${result.queryText}`);
-  console.log(`  Response: ${result.fulfillmentText}`);
-  if (result.intent) {
-    console.log(`  Intent: ${result.intent.displayName}`);
-  } else {
-    console.log(`  No intent matched.`);
-  }
+
+// importando o arquivo contendo as configs do dialogflow
+const configs = require('./nodejs-bot-test-oevy-16e4f71bbf45.json');
+
+// criando uma sessão para a aplicação de acordo com as credenciais
+const sessionClient = new dialogflow.SessionsClient({
+    projectId: configs.project_id,
+    credentials: { private_key: configs.private_key, client_email: configs.client_email }
+});
+
+// funcao para encapsular o envio de mensagens do telegram para o dialogflow
+async function sendMessage(chatId, message) {
+    // criando ou recuperando a sessão do usuário
+    const sessionPath = sessionClient.sessionPath(configs.project_id, chatId);
+
+    // objeto para montar o request para o dialogflow
+    const request = {
+        session: sessionPath,
+        queryInput: {}
+    };
+
+    // request para tipo texto
+    const textQueryInput = { text: { text: message, languageCode: 'pt-BR' } };
+
+    // request para tipo evento
+    const eventQueryInput = { event: { name: 'start', languageCode: 'pt-BR' } }
+
+    // verificando se a mensagem enviada foi um start caso seja monta um evento chamando a action 'start'
+    // lembrem-se que essa action precisa estar cadastrada no dialogflow para conseguirmos chamá-la
+    request.queryInput = message === '/start' ?  eventQueryInput : textQueryInput;
+
+    // respostas da requisição para o dialogflow
+    const responses = await sessionClient.detectIntent(request);
+
+    // resultado da resposta do dialogflow
+    const result = responses[0].queryResult;
+
+    // retornando objeto para ser utilizado no arquivo index.js
+    return { text: result.fulfillmentText, intent: result.intent.displayName, fields: result.parameters.fields };
 }
+
+// exportando a função sendMessage
+module.exports.sendMessage = sendMessage
--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const TelegramBot = require('node-telegram-bot-api')
+// importando a biblioteca de api do telegram 
+const TelegramBot = require('node-telegram-bot-api');
 
- 
-// substitua o valor abaixo pelo token de telegrama que você recebe de @BotFather
-const token = '1468331741:AAFqmDuh6fVArg_M3I3NIB0M7FetIZYdBOw';
- 
-// Crie um bot que usa 'polling' para buscar novas atualizações
-const bot = new TelegramBot(token, {polling: true});
- 
-// Corresponde a "/ echo [qualquer]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' é a mensagem recebida do telegrama
-   // 'match' é o resultado da execução do regexp acima no conteúdo do texto
-   // da mensagem
- 
-  const chatId = msg.chat.id;
-  const resp = match[1]; //"qualquer coisa" capturado
- 
-  // enviar de volta o "qualquer coisa" correspondente para o bate-papo
-  bot.sendMessage(chatId, resp);
-});
- 
-// Ouça qualquer tipo de mensagem. Existem diferentes tipos de
-// mensagens.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  
-  console.log(msg.text);
-  // envia uma mensagem para o chat confirmando o recebimento da mensagem
-  bot.sendMessage(chatId, 'Olá, Qual o seu nome?');
-  
+// importando nosso arquivo que faz a chamada para o dialogflow
+const dialogflow = require('./dialogflow');
+
+// importando nosso módulo que faz a busca no youtube
+const youtube = require('./youtube');
+
+// token recebido pelo bot father
+const token = 'COLOQUE O SEU TOKEN AQUI';
+
+// nova instância do telegram
+const bot = new TelegramBot(token, { polling: true });
+
+// escuta mensagens enviadas pelos usuários
+bot.on('message', async (msg) => {
+    
+    // id do chat do usuário
+    const chatId = msg.chat.id;
+
+    // resposta do dialogflow
+    const dfResponse = await dialogflow.sendMessage(chatId.toString(), msg.text);
+
+    // texto a partir da resposta do dialogflow
+    let textResponse = dfResponse.text;
+    
+    // verifica a intenção a partir da resposta do dialogflow
+    if (dfResponse.intent === 'Treino específico') {
+        // modifica o texto para os dados retornados a partir da busca realizada no youtube
+        // lembre-se que para acessar o campo corpo dentro de fields ele teve que ser definido como uma entidade no dialogflow
+        textResponse = await youtube.searchVideoURL(textResponse, dfResponse.fields.corpo.stringValue);
+    }
+    
+    // envio da mensagem para o usuário do telegram
+    bot.sendMessage(chatId, textResponse);
 });
